fad356c98e9b34b7bcd85c1d8a6b44c10e531c0a
Jogani Parit <103569209+parit9609@users.noreply.github.com>
2025-02-23 13:32:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -172,6 +172,8 @@ def scripts_js():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
+    port = int(os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)))
 
 
